364b2bffbfdb6dd1da55de4ff88572f534539ae8
pc1 <singhharshvisen16@gmail.com>
2022-09-17 12:07:34 +0530

    ryjrh

---
+++ dictionaryadd.py
@@ -0,0 +1,6 @@
+# wap to sum all the elements in dictionary
+
+
+dic1={"apple":100,"banana":50,"hello":12}
+sum1=sum(dic1.values())
+print(sum)
---
+++ readlinespec.py
@@ -0,0 +1,4 @@
+f=open("hello.txt","r")
+fc=f.readline()
+print(fc)
+f.close
