95deffbbddedf6a5a16ffacc3998c77728dfa679
pc1 <singhharshvisen16@gmail.com>
2022-09-19 12:09:38 +0530

    ryjrh

--- oops.py
+++ oops.py
@@ -1,12 +1,12 @@
-class student():
-    def __init__(self,sname,sroll,smob,scourse):
+class Student():
+    def __init__(self):
         self.sname=input(" enter the name:- ")
         self.sroll=int(input(" enter the roll no :-  "))
         self.smob= int(input(" enter the mobile number  :- "))
         self.scourse= input(" enter the course name :-  ")
 
-    def config(self):
-        print("config is :-",self.sname,self.sroll,self.smob,self.scourse)
+    def details(self):
+        print("Details are  :-",self.sname,self.sroll,self.smob,self.scourse)
 
-ss=student()
-ss.config()
+ss=Student()
+ss.details()
