6b95e67b3076051cc73cf0bc1173b35bdb955191
pc1 <singhharshvisen16@gmail.com>
2022-09-28 11:02:50 +0530

    ryjrh

--- multiplyer.py
+++ multiplyer.py
@@ -1,9 +1,10 @@
 class Number:
+
     Multiplier = None
 
     def __init__(self,x,y):
         self.x=x
-        self.y=yield
+        self.y=y
 
     #subpart 1
     def add(self):
@@ -15,11 +16,21 @@ class Number:
         return  a* cls.Multiplier
 
 
+    @staticmethod
+    def subtract(b,c):
+        return b-c
+
+    def value(self):
+        return(self.x,self.y)
 
 
 
 
-    obj=Number()
-    print(obj.add(5,10))
-    print(Number.Multiply(10))
+
+
+
+obj=Number(5,10)
+print(obj.add())
+print(Number.Multiply(10))
+print(number.subtract(100,1))
 
