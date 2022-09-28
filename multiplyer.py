0255722d52190df94ee2aa043296f2827a7dfd25
pc1 <singhharshvisen16@gmail.com>
2022-09-28 10:51:55 +0530

    ryjrh

---
+++ multiplyer.py
@@ -0,0 +1,25 @@
+class Number:
+    Multiplier = None
+
+    def __init__(self,x,y):
+        self.x=x
+        self.y=yield
+
+    #subpart 1
+    def add(self):
+        return self.x+self.y
+
+    #subpart 2
+    @classmethod
+    def multiply(cls,a):
+        return  a* cls.Multiplier
+
+
+
+
+
+
+    obj=Number()
+    print(obj.add())
+
+
