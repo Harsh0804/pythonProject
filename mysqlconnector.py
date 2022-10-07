1809d5b0ca802d4b6bda82a3d1cfbe1010519fef
pc1 <singhharshvisen16@gmail.com>
2022-10-07 10:28:38 +0530

    ryjrh

---
+++ mysqlconnector.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Admin123"
+)
+
+if mydb.is_connected():
+    print("connected")
+else:
+    print("not connected")
