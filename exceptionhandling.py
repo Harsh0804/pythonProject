9f01d0b93ba6fcbae0362fe16e9df147a921d365
SCMS <singhharshvisen16@gmail.com>
2022-10-13 14:01:56 +0530

    ryjrh

--- exceptionhandling.py
+++ exceptionhandling.py
@@ -1,1 +1,22 @@
-hello
+try:
+    print('try block')
+    x=int(input('Enter a number:-'))
+    y=int(input('Enter another number:-'))
+    z=x/y
+
+except ZeroDivisionError:
+    print('except ZeroDivisionError')
+    print('Division by 0 not accepted')
+
+
+else:
+    print('else block')
+    print('Division =',z)
+
+
+finally :
+    print('finally block')
+    x=0
+    y=0
+print('Out of try, except,else and finally block.')
+
