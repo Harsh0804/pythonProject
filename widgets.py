ed12116e91837fe267b56369cdaf24edf7d435cc
SCMS <singhharshvisen16@gmail.com>
2022-10-13 14:11:50 +0530

    ryjrh

---
+++ widgets.py
@@ -0,0 +1,13 @@
+# Q
+
+
+
+from tkinter import *
+window=Tk()
+window.title("9a")
+window.geometry("300x300")
+my_label=Label(window,text="python",bg="red",fg="white",font=('times',18),width=20)
+my_label.pack()
+
+
+window.mainloop()
