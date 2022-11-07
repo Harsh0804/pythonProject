293474702916770468578e5be81665cdbb23e16d
SCMS <singhharshvisen16@gmail.com>
2022-11-07 14:11:12 +0530

    ryjrh

--- armstrong.py
+++ armstrong.py
@@ -13,11 +13,18 @@ def armstrong(n):
       print(n,"is not an armstrong number")
 
 
-def pallindrome(n):
+def palindrome(n):
    temp(n)
    reverse=0
    while temp!=0:
       remainder=temp%10
-      reverse=reverse*10+remain
-n = input("enter the number")
+      reverse=reverse*10+remainder
+      temp=temp//10
+
+   if n==reverse:
+      print(n,"is palindrome")
+   else:
+      print(n,"Is not a palindrome")
+n = input("enter the number:- ")
 armstrong(n)
+palindrome(n)
